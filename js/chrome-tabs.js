a467474a80a24ab3b344700fcf27bcfbb235ba66
glitch128 <78228404+glitch128@users.noreply.github.com>
2022-10-08 11:31:45 -0700

    revert dropdown function

--- js/chrome-tabs.js
+++ js/chrome-tabs.js
@@ -386,7 +386,7 @@ function exampleFunc(event){
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function menu() {
+function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
